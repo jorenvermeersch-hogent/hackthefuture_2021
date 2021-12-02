faa94cb4373d88f06e1542704a1027182741dc7e
Kerem Yilmaz <kerem.yilmaz@student.hogent.be>
2021-12-02 11:23:58 +0100

    Case regel 61 done

---
+++ cases/case61.java
@@ -0,0 +1,14 @@
+package cases;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class case61 {
+    public static void main(String[] args) {
+        List<String> lijst = new ArrayList<String>(Arrays.asList("pmk", "mPnS", "P3ML", "sLAy", "kXQ", "ab80", "SbM", "bh6", "PB6","65p"));
+        System.out.println(lijst); //pitch
+        lijst.set(8, "P99p");
+        System.out.println(lijst); //pitch
+    }
+}
