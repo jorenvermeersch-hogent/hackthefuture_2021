cb8ba1f574c6c33cb740635fd4efba5a070ee370
Kerem Yilmaz <kerem.yilmaz@student.hogent.be>
2021-12-02 14:22:26 +0100

    Case 34 done(eerst vragen though)

---
+++ cases/case34.java
@@ -0,0 +1,28 @@
+package cases;
+
+import java.util.Arrays;
+
+public class case34 {
+    public static void main(String[] args) {
+        String zin = "T4amo0xQcXm1EnmexN5gFXFpiXkOT1hgdsBaCT2dOUVdXWtcLWWjnliBv";
+        int lengteZin = zin.length();
+       
+        String zinInAlphabetischeVolgOrde[],zinArr[];
+
+        zinArr = zin.split("");
+        zinInAlphabetischeVolgOrde = zin.split("");
+        Arrays.sort(zinInAlphabetischeVolgOrde);
+       
+        boolean isAlpha = true;
+        for (int i = 0; i < lengteZin; i++){
+            if (zinInAlphabetischeVolgOrde[i] != zinArr[i]){
+                isAlpha = false;
+                break;
+            }
+        }
+        if(isAlpha)
+            System.out.println("Y");
+        else
+            System.out.println("N");
+    }
+}
